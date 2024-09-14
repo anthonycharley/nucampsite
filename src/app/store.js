35ff290db77d116499f753cb3150561809e48103
Anthony Charley <anthonycharley01@gmail.com>
2024-09-13 21:46:23 -0500

    Add Middleware

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
 import { campsitesReducer } from '../features/campsites/campsitesSlice';
 import { commentsReducer } from '../features/comments/commentsSlice';
 import { partnersReducer } from '../features/partners/partnersSlice';
 import { promotionsReducer} from '../features/promotions/promotionsSlice';
+import logger from 'redux-logger';
 
 export const store = configureStore({
   reducer: {
@@ -12,4 +12,5 @@ export const store = configureStore({
     partners: partnersReducer,
     promotions: promotionsReducer
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat([logger])
 });
