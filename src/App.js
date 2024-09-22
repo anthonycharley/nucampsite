995e196be82d9314dfb5743f640d80579532f34a
Anthony Charley <anthonycharley01@gmail.com>
2024-09-22 13:11:23 -0500

    Async Partners

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { fetchCampsites } from './features/campsites/campsitesSlice';
+import { fetchPartners } from './features/partners/partnersSlice';
 import ContactPage from './pages/ContactPage';
 import HomePage from './pages/HomePage';
 import Header from './components/Header';
@@ -16,7 +17,9 @@ function App() {
 
   useEffect(() => {
     dispatch(fetchCampsites());
+    dispatch(fetchPartners());
 }, [dispatch]);
+
   return (
     <div className="App">
       <Header />
